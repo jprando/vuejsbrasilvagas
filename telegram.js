b5a562024f848d530afb4ca8c5d29320910528e8
Jeudi Prando Araujo <jeudi.araujo@el.com.br>
2020-07-28 02:35:46 -0300

    adjusts

--- telegram.js
+++ telegram.js
@@ -1,8 +1,6 @@
 const { Telegraf } = require('telegraf')
 const { BOT_TOKEN, CHAT_ID } = require('./config')
 
-const config = { disable_web_page_preview: true }
-
 if(!BOT_TOKEN) {
   console.error('TelegramError: 401: TELEGRAM_BOT_TOKEN environment is required')
   process.exit(1)
@@ -12,6 +10,8 @@ if(!CHAT_ID) {
   console.error('TelegramError: 400: TELEGRAM_CHAT_ID environment is required')
   process.exit(1)
 }
+
+const config = { disable_web_page_preview: true }
 
 const bot = new Telegraf(BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Procuro vagas de VueJS por ai e publico no grupo @vuejsbrasil'))
@@ -26,4 +26,3 @@ const telegramSendMessage = msg => {
 }
 
 module.exports = { telegramSendMessage }
-
